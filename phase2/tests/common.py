a8667138a0973542722dac0dc52cd7de872fa853
Oleg <olmoskalyov@lohika.com>
2018-09-03 01:51:53 +0300

    Adding the login and create issue tests

---
+++ phase2/tests/common.py
@@ -0,0 +1,6 @@
+
+import datetime
+
+
+def get_time_stamp():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
---
+++ phase2/tests/jira_api.py
@@ -0,0 +1,74 @@
+# import json
+
+class ApiBaseClass():
+    JIRA_HOST_URL = "http://jira.hillel.it:8080"
+
+    def get_body(self):
+        return self._body
+
+
+class ApiSession(ApiBaseClass):
+    endpoint_url = ApiBaseClass.JIRA_HOST_URL + "/rest/auth/1/session"
+
+    _body = {
+        "username": "$username",
+        "password": "$password"
+    }
+
+    def __init__(self, username, password):
+        self._body["username"] = username
+        self._body["password"] = password
+
+    # def get_body(self):
+    #     return self._body
+
+
+class ApiIssue(ApiBaseClass):
+
+    endpoint_url = ApiBaseClass.JIRA_HOST_URL + "/rest/api/2/issue"
+
+    _body = {
+        "fields": {
+            "project":
+                {
+                    "key": "$key"
+                },
+            "summary": "$summary",
+            "issuetype": {
+                "id": "$id"
+            },
+        }
+    }
+
+
+    def __init__(self, project_key, issuetype, summary):
+        self._body["fields"]["project"]["key"] = project_key
+        self._body["fields"]["issuetype"]["id"] = issuetype
+        self._body["fields"]["summary"] = summary
+
+    # def get_body(self):
+    #     return self._body
+
+
+
+    # _body = {
+    #     "fields": {
+    #         "project":
+    #             {
+    #                 "key": "$key"
+    #             },
+    #         "summary": "$summary",
+    #         "description": "$description",
+    #         "assignee": {
+    #             "name": "$assignee"
+    #         },
+    #         "issuetype": {
+    #             "id": "$id"
+    #         },
+    #         "priority": {
+    #             "name": "$priority"
+    #         }
+    #     }
+    # }
+
+
